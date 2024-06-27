dd3c47e20c4a75b4a3f325dd9f4e43848e8c6e7c
Michael Martin <mem101296@protonmail.com>
2024-06-27 12:49:58 -0400

    exit now works

--- rust_budget/src/main.rs
+++ rust_budget/src/main.rs
@@ -1,4 +1,4 @@
-use std::{arch::x86_64::_MM_EXCEPT_INEXACT, io};
+use std::{arch::x86_64::_MM_EXCEPT_INEXACT, io, process::exit};
 
 fn main() {
     println!("Welcome to rustBudget, a program to help you not go broke!");
@@ -8,12 +8,12 @@ fn main() {
 fn bill_loop() {
     let n = 0;
     while n < 1 {
-        let mut input = String::new();
+        let mut input = String::new(); //mutable string
         println!("What would you like to do?\n1. Add a bill\n2. Modify a bill\n3. Exit");
         io::stdin()
-            .read_line(&mut input)
+            .read_line(&mut input) //reads input
             .expect("Incorrect, please try again");
-        input = input.trim().to_string();
+        input = input.trim().to_string(); //trims it
 
         if input.eq("1") {
             //if input is equal to 1
@@ -22,6 +22,9 @@ fn bill_loop() {
         if input.eq("2") {
             //if input is equal to 2
             add_bill_to_bills();
+        }
+        if input.eq("3") {
+            exit(1);
         }
     }
 }
