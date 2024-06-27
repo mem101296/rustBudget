9ddbe1d9688d5f82f5195a7740465896f901cada
Michael Martin <mem101296@protonmail.com>
2024-06-27 12:20:56 -0400

    Added cargo project and some starting code

---
+++ rust_budget/src/main.rs
@@ -0,0 +1,40 @@
+use std::{arch::x86_64::_MM_EXCEPT_INEXACT, io};
+
+fn main() {
+    println!("Welcome to rustBudget, a program to help you not go broke!");
+    bill_loop();
+}
+
+fn bill_loop() {
+    let n = 0;
+    while n < 1 {
+        let mut input = String::new();
+        println!("What would you like to do?\n1. Add a bill\n2. Modify a bill\n3. Exit");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Incorrect, please try again");
+        input = input.trim().to_string();
+
+        if input.eq("1") {
+            //if input is equal to 1
+            take_bill();
+        }
+        if input.eq("2") {
+            //if input is equal to 2
+            add_bill_to_bills();
+        }
+    }
+}
+
+fn take_bill() {
+    let mut input = String::new(); //creating a mutable String called input
+    println!("Please enter a bill to add to your budget"); //outputting a test
+    io::stdin() //waiting for an input
+        .read_line(&mut input) //reading input
+        .expect("Failed to read line"); //error code
+    println!("Debugg: {}", input.trim()); //outputting input
+}
+
+fn add_bill_to_bills() {
+    //this is for an array
+}
