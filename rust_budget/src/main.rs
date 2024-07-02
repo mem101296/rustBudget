d1d8e9c576cbe785b20fc64308310ab100710fa6
Marty Kulma <user.email@site.thing>
2024-07-02 09:03:51 -0400

    Pass ndarray using mutable reference

--- rust_budget/src/main.rs
+++ rust_budget/src/main.rs
@@ -1,20 +1,22 @@
-use std::{arch::x86_64::_MM_EXCEPT_INEXACT, io, process::exit};
 use ndarray::prelude::*;
-use ndarray::{ArrayBase, Array, Array3};
+use ndarray::Array;
+use std::{io, process::exit};
 
 fn main() {
-    //let mut bills = Array::<f64, _>::zeros((3,4,5).f());
+    let mut bills = Array::<f64, _>::zeros((3, 4, 5).f());
     println!("Welcome to rustBudget, a program to help you not go broke!");
     //https://github.com/rust-ndarray/ndarray/blob/master/README-quick-start.md
-    //bills[[2,2,2]] = 15.0; //modify array at 2,2,2
-    //println!("{}", bills[[2,2,2]]); //output array at 2,2,2
-    //TODO Figure out how to pass arrays between functions
-    bill_loop();
+    bills[[2, 2, 2]] = 15.0; //modify array at 2,2,2
+    println!("{}", bills[[2, 2, 2]]); //output array at 2,2,2
+
+    // main() function still "owns" bills, and this allows bill_loop() to modify it
+    bill_loop(&mut bills);
+    // we can continue to use bills in main after this point
 }
 
-fn bill_loop() {
+fn bill_loop(bills: &mut Array<f64, Ix3>) {
     let n = 0;
-    let mut bills = Array::<f64, _>::zeros((3,4,5).f());
+    //let mut bills = Array::<f64, _>::zeros((3,4,5).f());
     while n < 1 {
         let mut input = String::new(); //mutable string
         println!("What would you like to do?\n1. Add a bill\n2. Modify a bill\n3. Exit");
@@ -25,29 +27,30 @@ fn bill_loop() {
 
         if input.eq("1") {
             //if input is equal to 1
-            take_bill(bills.clone());
+            take_bill(bills);
             //bills[[2,2,2]] = 15;
-            println!("{}", bills[[2,2,2]]);
+            println!("{}", bills[[2, 2, 2]]);
         }
         if input.eq("2") {
             //if input is equal to 2
             add_bill_to_bills();
         }
         if input.eq("3") {
+            println!("\n\ntotal = {}", bills.sum());
             exit(1);
         }
     }
 }
 
-fn take_bill<T>(mut A: Array::<f64, T>){
+fn take_bill(bills: &mut Array<f64, Ix3>) {
     let mut input = String::new(); //creating a mutable String called input
     println!("Please enter a bill to add to your budget"); //outputting a test
     io::stdin() //waiting for an input
         .read_line(&mut input) //reading input
         .expect("Failed to read line"); //error code
-    println!("Debugg: {}", input.trim()); //outputting input
-    A[[2,2,2]] = 16.0;
-    
+
+    println!("Debug: {}", input.trim()); //outputting input
+    bills[[2, 2, 2]] = 16.0;
 }
 
 fn add_bill_to_bills() {
